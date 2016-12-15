5bef1ea01ad940322032b2356ad0def134070531
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-15 18:10:28 +0530

    Initial commit
    update.sh, to --/update(s)/-- the dependencies for "ITPDB®" | Usage: $ sh update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# ==========================++== ITPDB® ==++==========================
+#    A manually curated dedicated ion transport proteins database.
+#
+#                                 CCPE
+#             Centre for Computational Protein Engineering
+#
+#                            Apache License
+#                    http://www.apache.org/licenses/
+#
+#        Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+#     Copyright © 2006 - 2016, Prabhat Kumar. All rights reserved.
+# Copyright © 2014 - 2016, Sequømics Corporation, All rights reserved.
+# ====================================================================
