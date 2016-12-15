9b8c30dc665e0fa940422b7b071586b9adb8c12d
Centre for Computational Protein Engineering <CCPE@users.noreply.github.com>
2016-12-15 18:50:11 +0530

    Initial commit
    grunt-contrib-clean — Clear files and directories.
    Usage: $ npm install grunt-contrib-clean --save-dev | Help: https://github.com/gruntjs/grunt-contrib-clean

---
+++ tasks/options/clean.js
@@ -0,0 +1,30 @@
+/*!
+ * -----------
+ * ITPDB®-Data
+ * -----------
+ * Data of ITPDB — A manually curated dedicated ion transport proteins database.
+ * _____________________________________________________________________________
+ *
+ * Grunt, http://gruntjs.com/ — The JavaScript Task Runner.
+ * _____________________________________________________________________________
+ *
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Sequømics Research [http://research.sequomics.com/].
+ * @copyright : Sequømics Corporation [http://sequomics.com/].
+ * _____________________________________________________________________________
+ *
+ * @date      : 10-Dec-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : grunt-contrib-clean
+ * @build     : SEED™ — Skövde
+ *              └────── A Sequømics Product — http://seed.sequomics.com/.
+ * _____________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "ITPDB®-Data".
+ * _____________________________________________________________________________
+ */
